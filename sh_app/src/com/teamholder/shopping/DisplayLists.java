66ebed4f140ee8bc7d81eff71e3a8423272dd2dc
Steve Holder <steve.holder@gmail.com>
2011-01-03 20:26:31 -0800

    working display of an html page

--- sh_app/src/com/teamholder/shopping/DisplayLists.java
+++ sh_app/src/com/teamholder/shopping/DisplayLists.java
@@ -2,12 +2,15 @@ package com.teamholder.shopping;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class DisplayLists extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("file:///android_asset/html/index.html");
     }
 }
