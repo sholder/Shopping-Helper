665072850cd08310eb1cc7eb714c9ebe35c6706c
Steve Holder <steve.holder@gmail.com>
2011-01-10 21:37:39 -0800

    trying to load a web page as list... not working yet

--- sh_app/src/com/teamholder/shopping/DisplayLists.java
+++ sh_app/src/com/teamholder/shopping/DisplayLists.java
@@ -1,16 +1,42 @@
 package com.teamholder.shopping;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class DisplayLists extends Activity {
+public class DisplayLists extends Activity implements OnClickListener {
+	
+	static final String LIST_URL = "com.teamholder.shopping.ListUrl";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        //WebView webview = new WebView(this);
-        //setContentView(webview);
-        //webview.loadUrl("file:///android_asset/html/index.html");
+        installButtonListeners();
     }
+    
+    private void installButtonListeners() {
+    	findViewById(R.id.clean_button).setOnClickListener(this);
+    	findViewById(R.id.dirty_button).setOnClickListener(this);
+    	findViewById(R.id.full_button).setOnClickListener(this);
+    }
+
+	@Override
+	public void onClick(View v) {
+		Intent i = new Intent(this, ShowList.class);
+		switch (v.getId()) {
+		case R.id.dirty_button:
+			i.putExtra(LIST_URL, "file://android_asset/html/dirty_dozen.html");
+			break;
+		default:
+			return;
+		}
+		Log.i("DisplayLists", "about to start activity");
+		startActivity(i);
+		Log.i("DisplayLists", "activity started");
+	}
 }
