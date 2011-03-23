a8e98ae6fdf6da141d8043d9187f4841786448c7
Steve Holder <steve.holder@gmail.com>
2011-03-22 20:46:26 -0700

    fleshed out tests, added stub for ShowListTest

--- sh_appTest/src/com/teamholder/shopping/test/DisplayListsTest.java
+++ sh_appTest/src/com/teamholder/shopping/test/DisplayListsTest.java
@@ -3,6 +3,7 @@ package com.teamholder.shopping.test;
 import android.content.Intent;
 import android.test.ActivityUnitTestCase;
 import android.view.View;
+import android.widget.Button;
 
 import com.teamholder.shopping.DisplayLists;
 import com.teamholder.shopping.R;
@@ -29,5 +30,41 @@ public class DisplayListsTest extends ActivityUnitTestCase<DisplayLists> {
 		String targetUrl = intent.getExtras().getString(DisplayLists.LIST_URL);
 		assertNotNull(targetUrl);
 		assertEquals("file:///android_asset/html/dirty_dozen.html", targetUrl);
+	}
+	
+	public void testCleanButton() {
+		startActivity(new Intent(), null, null);
+
+		View v = getActivity().findViewById(R.id.clean_button);
+		getActivity().onClick(v);
+		
+		Intent intent = getStartedActivityIntent();
+		assertNotNull(intent);
+		String targetUrl = intent.getExtras().getString(DisplayLists.LIST_URL);
+		assertNotNull(targetUrl);
+		assertEquals("file:///android_asset/html/clean_15.html", targetUrl);		
+	}
+	
+	public void testFullButton() {
+		startActivity(new Intent(), null, null);
+
+		View v = getActivity().findViewById(R.id.full_button);
+		getActivity().onClick(v);
+		
+		Intent intent = getStartedActivityIntent();
+		assertNotNull(intent);
+		String targetUrl = intent.getExtras().getString(DisplayLists.LIST_URL);
+		assertNotNull(targetUrl);
+		assertEquals("file:///android_asset/html/full_list.html", targetUrl);
+	}
+	
+	public void testUnknownButton() {
+		startActivity(new Intent(), null, null);
+		
+		View dummyView = new Button(getActivity().getBaseContext());
+		getActivity().onClick(dummyView);
+		
+		// should not have started anything
+		assertNull(getStartedActivityIntent());
 	}
 }
---
+++ sh_appTest/src/com/teamholder/shopping/test/ShowListTest.java
@@ -0,0 +1,16 @@
+package com.teamholder.shopping.test;
+
+import android.test.ActivityUnitTestCase;
+
+import com.teamholder.shopping.ShowList;
+
+public class ShowListTest extends ActivityUnitTestCase<ShowList> {
+
+	public ShowListTest() {
+		super(ShowList.class);
+	}
+	
+	public void testBackButton() {
+		// TODO(sholder) implement
+	}
+}
